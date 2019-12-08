035826b6ae088d89f64396e4b56fdd5119b18f19
eridhobffry <eridhobffry@github.com>
2019-12-08 20:47:03 +0100

    New Logo Example

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from "styled-components";
 
-import logo from "../assets/img/logo-min.png";
+// import logo from "../assets/img/logo-min.png";
+import logo from "../assets/img/example-logo.png";
 
 const Brand = () => {
   return (
@@ -12,6 +13,7 @@ const Brand = () => {
 export default Brand
 
 const Image = styled.img`
+  margin-left: -22px !important;
   height: 85%;
   margin: auto 0;
   border-radius: 50px;
